bc6f4edd0cda55a0347a63de0dcecf22a117dd04
PiotrDuma <piotrduma2014@gmail.com>
2022-09-09 23:03:33 +0200

    add token auth interface

--- src/main/java/com/github/PiotrDuma/imageshack/tools/TokenAuthService/TokenAuthService.java
+++ src/main/java/com/github/PiotrDuma/imageshack/tools/TokenAuthService/TokenAuthService.java
@@ -1,5 +1,14 @@
 package com.github.PiotrDuma.imageshack.tools.TokenAuthService;
 
-public interface TokenAuthService {
+import com.github.PiotrDuma.imageshack.tools.TokenAuthService.TokenAuthDomain.TokenAuthType;
+import java.util.Optional;
+import java.util.stream.Stream;
 
+public interface TokenAuthService {
+  String createToken(String email, TokenAuthType tokenAuthType, int activeTimeInMinutes);
+  boolean isTokenActive(String email, String token, TokenAuthType tokenAuthType);
+  void deleteToken(String email, TokenAuthType tokenAuthType);
+  Optional<TokenAuthDTO> findToken(String email, TokenAuthType tokenAuthType);
+  Stream<TokenAuthDTO> getAllTokensByEmail(String email);
+  Stream<TokenAuthDTO> getAllExpiredTokens();
 }
