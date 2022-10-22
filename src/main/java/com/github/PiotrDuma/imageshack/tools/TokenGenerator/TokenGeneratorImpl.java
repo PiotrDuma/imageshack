9d0606ecd819f547cd2d1f8e94e8b72492e09c20
PiotrDuma <piotrduma2014@gmail.com>
2022-10-22 22:16:09 +0200

    fix annotation

--- src/main/java/com/github/PiotrDuma/imageshack/tools/TokenGenerator/TokenGeneratorImpl.java
+++ src/main/java/com/github/PiotrDuma/imageshack/tools/TokenGenerator/TokenGeneratorImpl.java
@@ -3,8 +3,9 @@ package com.github.PiotrDuma.imageshack.tools.TokenGenerator;
 import java.security.SecureRandom;
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
-@Qualifier("tokenGenerator")
+@Service("tokenGenerator")
 class TokenGeneratorImpl implements TokenGenerator{
   //source https://stackoverflow.com/posts/56628391/revisions
   private final SecureRandom secureRandom;
