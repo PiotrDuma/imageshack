9ae612f2155c53df3dba1e8f4eeba9ee2792a041
PiotrDuma <piotrduma2014@gmail.com>
2022-11-08 01:38:19 +0100

    fix missing clock bean

--- src/main/java/com/github/PiotrDuma/imageshack/ImageshackApplication.java
+++ src/main/java/com/github/PiotrDuma/imageshack/ImageshackApplication.java
@@ -2,8 +2,10 @@ package com.github.PiotrDuma.imageshack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"java.time.Clock"})
 public class ImageshackApplication {
 
 	public static void main(String[] args) {
