7ccc19b311c3af528da2af03bf738132d072f48d
PiotrDuma <piotrduma2014@gmail.com>
2022-12-02 19:26:29 +0100

    init registration controller

--- src/main/java/com/github/PiotrDuma/imageshack/api/registration/AppUserDTO.java
+++ src/main/java/com/github/PiotrDuma/imageshack/api/registration/AppUserDTO.java
@@ -1,13 +1,34 @@
 package com.github.PiotrDuma.imageshack.api.registration;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class AppUserDTO {
+  private static final String USERNAME_LENGTH = "Username must be between 3 and 20 characters";
+  private static final String PASSWORD_LENGTH_MIN = "Password must have at least 8 characters";
+  private static final String PASSWORD_LENGTH_MAX = "Password must have less than 32 characters";
+  private static final String EMAIL_LENGTH = "Email address is too long";
+
+  @NotBlank
+  @Size(min = 3, max = 32, message = USERNAME_LENGTH)
   private String username;
+  @Email
+  @NotBlank
+  @Size(max = 64, message = EMAIL_LENGTH)
   private String email;
+  @NotBlank
+  @Size(min = 8, message = PASSWORD_LENGTH_MIN)
+  @Size(max = 32, message = PASSWORD_LENGTH_MAX)
   private String password;
 
-  public AppUserDTO(String username, String email) {
+  public AppUserDTO() {
+  }
+
+  public AppUserDTO(String username, String email, String password) {
     this.username = username;
     this.email = email;
+    this.password = password;
   }
 
   public String getUsername() {
@@ -24,5 +45,13 @@ public class AppUserDTO {
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
   }
 }
--- src/main/java/com/github/PiotrDuma/imageshack/api/registration/RegistrationController.java
+++ src/main/java/com/github/PiotrDuma/imageshack/api/registration/RegistrationController.java
@@ -1,25 +1,44 @@
 package com.github.PiotrDuma.imageshack.api.registration;
 
+import com.github.PiotrDuma.imageshack.tools.validators.Validator;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/register")
 public class RegistrationController {
-  private final RegistrationService registrationService;
+  private final Validator emailValidator;
+  private final Validator usernameValidator;
+  private final Validator passwordValidator;
 
-  public RegistrationController(RegistrationService registrationService) {
-    this.registrationService = registrationService;
+  @Autowired
+  public RegistrationController(
+      @Qualifier("emailValidator") Validator emailValidator,
+      @Qualifier("usernameValidator") Validator usernameValidator,
+      @Qualifier("passwordValidator") Validator passwordValidator){
+    this.emailValidator = emailValidator;
+    this.usernameValidator = usernameValidator;
+    this.passwordValidator = passwordValidator;
   }
 
-  @GetMapping(value = "/register")
-  public String getRegistrationPage(){
+  @GetMapping
+  public String getRegistrationPage(Model model){
+    model.addAttribute("dto", new AppUserDTO());
     return "register";
   }
 
-  @PostMapping(value = "/register")
-  public String registerUser(){ //TODO: post mapping atributes.
-    this.registrationService.sendAccountAuthenticationToken("any");
+  @PostMapping
+  public String registerUser(@Valid @ModelAttribute("dto") AppUserDTO dto,
+                       BindingResult bindingResult, Model model){
+    //TODO:validate input.
     return "register";
   }
 }
