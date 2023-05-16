b489b916aef84e12813f1f8e890ee247b465c339
PiotrDuma <piotrduma2014@gmail.com>
2023-05-16 15:58:58 +0200

    set credentials authentication before UserDetails' checks

--- src/main/java/com/github/PiotrDuma/imageshack/api/login/CustomUserDetailsAuthenticationProvider.java
+++ src/main/java/com/github/PiotrDuma/imageshack/api/login/CustomUserDetailsAuthenticationProvider.java
@@ -2,22 +2,43 @@ package com.github.PiotrDuma.imageshack.api.login;
 
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsChecker;
 
 public class CustomUserDetailsAuthenticationProvider extends DaoAuthenticationProvider {
-  private UserDetailsChecker checker;
+  private UserDetailsChecker postChecker;
+  private UserDetailsChecker preChecker;
   public CustomUserDetailsAuthenticationProvider() {
     super();
-    this.checker = new CustomPreAuthenticationChecks();
-    super.setPreAuthenticationChecks(checker);
+    this.preChecker = new CustomPreAuthenticationChecks();
+    this.postChecker = new CustomPostAuthenticationChecks();
+    super.setPreAuthenticationChecks(preChecker);
+    super.setPostAuthenticationChecks(postChecker);
   }
 
-  private class CustomPreAuthenticationChecks implements UserDetailsChecker{
+  private class CustomPreAuthenticationChecks implements UserDetailsChecker {
 
     /**
-     * Customize exceptions thrown with chosen UserDetail's values.
+     * Avoid UserDetails' flag checks before credentials' authentication by
+     * {@link org.springframework.security.authentication.dao.DaoAuthenticationProvider#additionalAuthenticationChecks(
+     * UserDetails, UsernamePasswordAuthenticationToken)} in
+     * {@link org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider#authenticate(
+     * Authentication)} method.
+     */
+    @Override
+    public void check(UserDetails user) {
+      //DO NOTHING
+    }
+  }
+
+  private class CustomPostAuthenticationChecks implements UserDetailsChecker {
+
+    /**
+     * Validate UserDetails' flags removed from {@link CustomPreAuthenticationChecks#check(UserDetails)}
+     * and optionally throw appropriate exceptions.
      */
     @Override
     public void check(UserDetails user) {
@@ -25,7 +46,7 @@ public class CustomUserDetailsAuthenticationProvider extends DaoAuthenticationPr
         logger.debug("Failed to authenticate since user account is locked");
         throw new LockedException("User account is locked");
       }
-      if(!user.isEnabled()){
+      if (!user.isEnabled()) {
         logger.debug("Failed to authenticate since user account is disabled");
         throw new DisabledException("User is disabled");
       }
--- src/test/java/com/github/PiotrDuma/imageshack/api/login/CustomUserDetailsAuthenticationProviderTest.java
+++ src/test/java/com/github/PiotrDuma/imageshack/api/login/CustomUserDetailsAuthenticationProviderTest.java
@@ -3,54 +3,68 @@ package com.github.PiotrDuma.imageshack.api.login;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Field;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsChecker;
 
 @ExtendWith(MockitoExtension.class)
 class CustomUserDetailsAuthenticationProviderTest {
-
-  private DaoAuthenticationProvider provider;
-  private UserDetailsChecker checker;
+  //invoke by field
+  private UserDetailsChecker preChecker;
+  private UserDetailsChecker postChecker;
 
   @BeforeEach
   void setUp() throws Exception{
-    this.provider = new CustomUserDetailsAuthenticationProvider();
-    Field field = provider.getClass().getDeclaredField("checker");
-    field.setAccessible(true);
-    this.checker = (UserDetailsChecker)field.get(provider);
+    DaoAuthenticationProvider provider = new CustomUserDetailsAuthenticationProvider();
+
+    Field fieldPreChecker = provider.getClass().getDeclaredField("preChecker");
+    fieldPreChecker.setAccessible(true);
+    this.preChecker = (UserDetailsChecker)fieldPreChecker.get(provider);
+
+    Field fieldPostChecker = provider.getClass().getDeclaredField("postChecker");
+    fieldPostChecker.setAccessible(true);
+    this.postChecker = (UserDetailsChecker)fieldPostChecker.get(provider);
   }
 
   @Test
-  void checkShouldThrowDisabledExceptionWhenUserIsNotEnabled(){
+  void postCheckShouldThrowDisabledExceptionWhenUserIsNotEnabled(){
     UserDetails user = mock(UserDetails.class);
-    Authentication auth = mock(Authentication.class);
 
     when(user.isEnabled()).thenReturn(false);
     when(user.isAccountNonLocked()).thenReturn(true);
 
-    Exception ex = assertThrows(DisabledException.class, () -> this.checker.check(user));
+    Exception ex = assertThrows(DisabledException.class, () -> this.postChecker.check(user));
     assertEquals("User is disabled", ex.getMessage());
   }
 
   @Test
-  void checkShouldThrowLockedExceptionWhenUserIsLocked(){
+  void postCheckShouldThrowLockedExceptionWhenUserIsLocked(){
     UserDetails user = mock(UserDetails.class);
-    Authentication auth = mock(Authentication.class);
 
     when(user.isAccountNonLocked()).thenReturn(false);
 
-    Exception ex = assertThrows(LockedException.class, () -> this.checker.check(user));
+    Exception ex = assertThrows(LockedException.class, () -> this.postChecker.check(user));
     assertEquals("User account is locked", ex.getMessage());
+  }
+
+  @Test
+  void preCheckShouldDoNothing(){
+    UserDetails user = mock(UserDetails.class);
+    UserDetailsChecker spy = Mockito.spy(preChecker);
+
+    spy.check(user);
+    verify(spy, times(1)).check(user);
   }
 }
