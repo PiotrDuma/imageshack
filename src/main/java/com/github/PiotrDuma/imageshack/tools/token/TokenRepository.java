1076bd63f7e123d1fcbb7131b59846609c1dce91
PiotrDuma <piotrduma2014@gmail.com>
2024-02-11 22:54:38 +0100

    update: encapsulate repo

--- src/main/java/com/github/PiotrDuma/imageshack/tools/token/TokenRepository.java
+++ src/main/java/com/github/PiotrDuma/imageshack/tools/token/TokenRepository.java
@@ -3,7 +3,7 @@ package com.github.PiotrDuma.imageshack.tools.token;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface TokenRepository {
+interface TokenRepository {
     TokenEntity save(TokenEntity token);
     Optional<TokenEntity> findByPublicId(UUID publicId);
 }
